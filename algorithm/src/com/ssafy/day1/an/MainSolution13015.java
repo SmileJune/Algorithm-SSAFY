8b160d7c32ac05e7700c2703ddc820581f49ce2b
DESKTOP-3O24HHM\hitedin <hitedin@gmail.com>
2022-02-08 22:30:25 +0900

    Feat: "1일차 백준"
    날먹 문제로 시스템 좀 보고, 한단계 올린 별찍기에서 시간이 1시간이 넘어 버렸습니다.
    단순 카운팅 문제는 배제하고 머리 살짝 손 살짝 쓰는 문제로 방향 잡겠습니다.

---
+++ algorithm/src/com/ssafy/day1/an/MainSolution13015.java
@@ -0,0 +1,73 @@
+package com.ssafy.day1.an;
+
+import java.util.Scanner;
+
+public class MainSolution13015 { // 13015 별찍기
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		for (int i = 0; i < num * 2 - 1; i++) {
+			if (i == 0 || i == num * 2 - 2) {
+				// case1 : 맨위, 맨아래 - 별갯수 만큼 출력과 사이 공백 계산해서 넣기
+				for (int j = 0; j < num; j++) {
+					System.out.print("*");
+				}
+				for (int j = 0; j < num * 2 - 3; j++) {
+					System.out.print(" ");
+				}
+				for (int j = 0; j < num; j++) {
+					System.out.print("*");
+				}
+				System.out.print("\n");
+			} else if (i == num - 1) {
+				// case2 : num - 1, 즉 0을 기준으로 엑스에 가운데 별 3개 값
+				for (int j = 0; j < num - 1; j++) {
+					System.out.print(" ");
+				}
+				System.out.print("*"); // 첫번째 별은 1번 열의 num번째 랑 같은 위치
+				for (int j = 0; j < num - 2; j++) {
+					System.out.print(" ");
+				}
+				System.out.print("*"); // 공백은 num-2씩 유지됨.
+				for (int j = 0; j < num - 2; j++) {
+					System.out.print(" ");
+				}
+				System.out.print("*\n");
+			} else {
+				// case3 : 별을 4개씩찍는데, 홀수 별과 짝수별의 차이는 num-2만큼 씩
+				for (int j = 0; j < (num - 1) - Math.abs(num - 1 - i); j++) {
+					System.out.print(" ");
+				} // 1/4부분 먼저 그려보고, 절대값으로 반대쪽 그리기
+				System.out.print("*");
+				for (int j = 0; j < num - 2; j++) {
+					System.out.print(" ");
+				}
+				System.out.print("*");
+				for (int j = 0; j < 2 * Math.abs((num - 1) - i) - 1; j++) {
+					System.out.print(" ");
+				}
+				System.out.print("*");
+				for (int j = 0; j < num - 2; j++) {
+					System.out.print(" ");
+				}
+				System.out.print("*\n");
+			}
+		}
+		sc.close();
+	}
+}
+//***   ***
+// * * * *
+//  * * *
+// * * * *
+//***   ***  
+//
+//*****       *****
+// *   *     *   *
+//  *   *   *   *
+//   *   * *   *
+//    *   *   *
+//   *   * *   *
+//  *   *   *   *
+// *   *     *   *
+//*****       *****
---
+++ algorithm/src/com/ssafy/day1/an/MainSolution2438.java
@@ -0,0 +1,16 @@
+package com.ssafy.day1.an;
+
+import java.util.Scanner;
+
+public class MainSolution2438 { // 2438 날먹
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		String s = "*";
+		for(int i = 0 ;i < n; i++) {
+			System.out.println(s);
+			s += '*';
+		}
+		sc.close();
+	}
+}
