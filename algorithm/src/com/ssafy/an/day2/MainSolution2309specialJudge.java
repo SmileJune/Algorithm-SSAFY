d9b8a13a456ae505b300c69dd5694df72193de01
DESKTOP-3O24HHM\hitedin <hitedin@gmail.com>
2022-02-09 18:59:34 +0900

    Feat : "2일차 백준"
    2309번 스페샬 저지 완료했습니다~~~~~~~~~
    맛잇는 저녁 되세요~~~~~~

---
+++ algorithm/src/com/ssafy/an/day2/MainSolution2309specialJudge.java
@@ -0,0 +1,53 @@
+package com.ssafy.an.day2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class MainSolution2309specialJudge {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		;
+		int[] arr = new int[9];
+		for (int i = 0; i < 9; i++)
+			arr[i] = Integer.parseInt(br.readLine());
+		// 거꾸로 해봅시다.
+		int sum9 = 0;
+		for (int i = 0; i < 9; i++) {
+			sum9 += arr[i];
+		}
+		bubbleSort(arr);
+		int num = sum9 - 100;
+
+		int idx1 = 0;
+		int idx2 = 0;
+
+		outer: for (int i = 0; i < 8; i++) {
+			for (int j = i + 1; j < 9; j++) {
+				if (arr[i] + arr[j] == num) {
+					idx1 = i;
+					idx2 = j;
+					break outer;
+				}
+			}
+		}
+		for (int i = 0; i < 9; i++) {
+			if (i != idx1 && i != idx2)
+				System.out.println(arr[i]);
+		}
+		br.close();
+	}
+
+	private static void bubbleSort(int[] arr) {
+		for (int i = 8; i >= 0; i--) {
+			for (int j = 0; j < i; j++) {
+				if (arr[j] > arr[j + 1]) {
+					int temp = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = temp;
+				}
+			}
+		}
+
+	}
+}
