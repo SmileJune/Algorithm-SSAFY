aa389291b7dd162091e193ace842df429f841630
SmileJun <godhkekf24@inha.edu>
2022-02-15 00:48:26 +0900

    Feat: "day7 및 수업 문제풀이코드"

---
+++ algorithm/src/com/ssafy/yun/Baek_2675.java
@@ -0,0 +1,40 @@
+package com.ssafy.yun;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Baek_2675 {
+
+	public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	public static StringTokenizer st;
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		int T = Integer.parseInt(br.readLine());
+		for (int tc = 1; tc <= T; tc++) {
+			solution();
+		}
+		bw.close();
+	}
+
+	public static void solution() throws IOException {
+		st = new StringTokenizer(br.readLine());
+		int cnt = Integer.parseInt(st.nextToken());
+		String str = st.nextToken();
+
+		for (int i = 0; i < str.length(); i++) {
+			char ch = str.charAt(i);
+			for (int j = 0; j < cnt; j++) {
+				bw.write(ch);
+			}
+		}
+
+		bw.write("\n");
+
+	}
+
+}
