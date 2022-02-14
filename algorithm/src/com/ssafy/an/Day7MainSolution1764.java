4b87efaee15ab21659a6779588ff61cc6a86900b
DESKTOP-3O24HHM\hitedin <hitedin@gmail.com>
2022-02-14 12:37:49 +0900

    Feat : "7일차 점심 백준"
    정렬 달리기

---
+++ algorithm/src/com/ssafy/an/Day7MainSolution1764.java
@@ -0,0 +1,43 @@
+package com.ssafy.an;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class Day7MainSolution1764 { // 1764 듣보잡
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+
+		Set<String> set = new HashSet<>();
+		List<String> list = new ArrayList<>();
+		while (N-- > 0) {
+			String name = br.readLine();
+			set.add(name);
+		}
+		while (M-- > 0) {
+			String name = br.readLine();
+			if (set.contains(name)) {
+				list.add(name);
+			}
+		}
+		Collections.sort(list);
+
+		StringBuilder ans = new StringBuilder();
+		ans.append(list.size()).append('\n');
+		for (String str : list) {
+			ans.append(str).append('\n');
+		}
+
+		System.out.println(ans);
+		br.close();
+	}
+}
