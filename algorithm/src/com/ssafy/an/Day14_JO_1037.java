e24dfedc98d7df14d92d8e1292a080e0b47ad51f
DESKTOP-3O24HHM\hitedin <hitedin@gmail.com>
2022-02-21 15:26:54 +0900

    Feat : "14일차 정올 쉬는시간"
    정올은 테스트 케이스 뭐 틀렸는 지 알려주는데, 이게 이렇게 풀어도 되는 지는 모르겠네요....

---
+++ algorithm/src/com/ssafy/an/Day14_JO_1037.java
@@ -0,0 +1,55 @@
+package com.ssafy.an;
+
+import java.util.Scanner;
+
+public class Day14_JO_1037 { // 1037 오류교정
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int[][] arr = new int[N][N];
+
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				arr[i][j] = sc.nextInt();
+			}
+		}
+		String ans = "Corrupt";
+
+		if (check(arr, N)) {
+			ans = "OK";
+		} else {
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < N; j++) {
+					int tmp = arr[i][j];
+					arr[i][j] = (arr[i][j] + 1) % 2;
+					if (check(arr, N)) {
+						ans = "Change bit (" + (i + 1) + "," + (j + 1) + ")";
+						break;
+					}
+					arr[i][j] = tmp;
+				}
+			}
+		}
+
+		System.out.println(ans);
+		sc.close();
+	}
+
+	private static boolean check(int[][] arr, int N) {
+		int rsum = 0;
+		int csum = 0;
+		for (int i = 0; i < N; i++) {
+			rsum = 0;
+			csum = 0;
+			for (int j = 0; j < N; j++) {
+				rsum += arr[i][j]; // 행 고정 합
+				csum += arr[j][i]; // 열 고정 합
+			}
+			if (rsum % 2 != 0 || csum % 2 != 0) {
+				return false;
+			}
+		}
+		// 나오면 합이 모두 짝수
+		return true;
+	}
+}
