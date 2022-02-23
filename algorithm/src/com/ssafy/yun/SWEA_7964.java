e79db1d9e23150348de707c73bd987fa743b1b0d
SmileJun <godhkekf24@inha.edu>
2022-02-23 22:59:49 +0900

    Feat: "yun_day16"
    어제 깃 문제로 안올라간 것까지 올렸습니다.
    사실 이미 올라가서 Unstaged Changes에도 안잡혀서 새로 풀어서 제출합니다. 대체 어딨니 너희

---
+++ algorithm/src/com/ssafy/yun/SWEA_7964.java
@@ -0,0 +1,44 @@
+package com.ssafy.yun;
+
+import java.util.Scanner;
+
+public class SWEA_7964 {
+
+	static Scanner sc = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		int T = sc.nextInt();
+		for (int tc = 1; tc <= T; tc++) {
+			System.out.printf("#%d %d%n", tc, solution());
+		}
+	}
+
+	static int solution() {
+		int N = sc.nextInt();
+		int limit = sc.nextInt();
+
+		int[] arr = new int[N + 2];
+		for (int i = 1; i <= arr.length - 2; i++) {
+			arr[i] = sc.nextInt();
+		}
+		arr[0] = 1;
+		arr[arr.length - 1] = 1;
+
+		int len = 0;
+		int cnt = 0;
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] == 0) {
+				len++;
+			}
+			if (arr[i] == 1) {
+				len = 0;
+			}
+			if (len == limit) {
+				len = 0;
+				cnt++;
+			}
+		}
+		return cnt;
+	}
+
+}
