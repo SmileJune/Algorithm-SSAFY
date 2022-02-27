e0ed1b3a3d7bdb77938f8ed9453766c11b806d1a
SmileJun <godhkekf24@inha.edu>
2022-02-28 00:16:31 +0900

    Revert "Merge pull request #210 from SmileJune/hotfix_merge_revert_test"
    This reverts commit d37a814936f89fcc33c3fb1f096f18a716edb5a7, reversing
    changes made to 8c6c8c9281906d451c2eead9158506eba72f52e9.

--- algorithm/src/com/ssafy/yun/Test.java
+++ algorithm/src/com/ssafy/yun/Test.java
@@ -1,21 +1,15 @@
 package com.ssafy.yun;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 public class Test {
-	
-	public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-	public static StringTokenizer st;
 
 	public static void main(String[] args) {
-		boolean[] isNotPrime = new boolean[10000000];
-		isNotPrime[0] = isNotPrime[1] = true;
-		
+		char[][] map = new char[1][1];
 
+		System.out.println("^v<>".indexOf('<'));
 	}
+
 }
