4cbb2b7b60f3508f3a76c5bc1fe380dcdae52a29
hsh102103 <ghkdtnghks147@gmail.com>
2022-03-02 04:43:19 +0900

    Feat "22일차 문제풀이"

---
+++ algorithm/src/com/ssafy/hwang/Day22_BOJ_10926.java
@@ -0,0 +1,12 @@
+package com.ssafy.hwang;
+
+import java.util.Scanner;
+
+public class Day22_BOJ_10926 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		StringBuilder sb = new StringBuilder();
+		sb.append(sc.next());
+		System.out.println(sb.toString()+"??!");
+	}
+}
