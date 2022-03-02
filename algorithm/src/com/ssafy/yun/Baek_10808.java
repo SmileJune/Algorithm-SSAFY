fa90890e2bf445e1cdd2a87537751b7b7defc235
SmileJun <godhkekf24@inha.edu>
2022-03-02 12:38:05 +0900

    Feat: "윤일준_day23"
    정처기에 집중을 해야해서 야무지지 못한 문제들을 조금 포함했습니다.

---
+++ algorithm/src/com/ssafy/yun/Baek_10808.java
@@ -0,0 +1,23 @@
+package com.ssafy.yun;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Baek_10808 {
+
+	static Scanner sc = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		String str = sc.nextLine();
+
+		int[] arr = new int[26];
+		for (int i = 0; i < str.length(); i++) {
+			char ch = str.charAt(i);
+			arr[ch - 'a']++;
+		}
+
+		String ans = Arrays.toString(arr).replaceAll(",", "");
+		System.out.println(ans.substring(1, ans.length() - 1));
+	}
+
+}
