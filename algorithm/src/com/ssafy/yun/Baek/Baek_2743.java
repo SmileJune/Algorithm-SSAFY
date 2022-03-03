d6cb0dcbfafec2b5ae98ee76184df40789963ded
SmileJun <godhkekf24@inha.edu>
2022-03-03 20:45:44 +0900

    Feat: "윤일준_day24"

---
+++ algorithm/src/com/ssafy/yun/Baek/Baek_2743.java
@@ -0,0 +1,12 @@
+package com.ssafy.yun.Baek;
+
+import java.util.Scanner;
+
+public class Baek_2743 {
+
+	static Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) {
+		System.out.println(sc.nextLine().length());
+	}
+
+}
