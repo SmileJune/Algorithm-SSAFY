9f2f938f0713479634ad102b84de04efd038bdd8
SmileJun <godhkekf24@inha.edu>
2022-03-05 23:14:24 +0900

    Feat: "윤일준_day26"

---
+++ algorithm/src/com/ssafy/yun/Baek/Baek_10171.java
@@ -0,0 +1,13 @@
+package com.ssafy.yun.Baek;
+
+public class Baek_10171 {
+
+	public static void main(String[] args) {
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( \')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+
+	}
+
+}
