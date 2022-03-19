afef46388fae39b41c968103f7a392557b0e5f6d
hsh102103 <54360785+hsh102103@users.noreply.github.com>
2022-03-19 21:42:26 +0900

    Update BOJ_2075_박성현.java
    Feat: "Day39"

--- algorithm/src/com/ssafy/namja/BOJ_2075_박성현.java
+++ algorithm/src/com/ssafy/namja/BOJ_2075_박성현.java
@@ -7,7 +7,6 @@ public class Main {
     static int n;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
         int N = sc.nextInt();
         heap = new int[N * N + 1];
         
