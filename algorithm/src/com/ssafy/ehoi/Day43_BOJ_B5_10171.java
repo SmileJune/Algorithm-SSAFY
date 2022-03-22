0bed2a6cd69f435664d72f8bddc274e69b5918aa
belle park <ehoi.loveyourself@gmail.com>
2022-03-23 02:40:45 +0900

    Feat. 43일차

---
+++ algorithm/src/com/ssafy/ehoi/Day43_BOJ_B5_10171.java
@@ -0,0 +1,11 @@
+package com.ssafy.ehoi;
+
+public class Day43_BOJ_B5_10171 {
+	public static void main(String[] args) {
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+		
+	}
+}
