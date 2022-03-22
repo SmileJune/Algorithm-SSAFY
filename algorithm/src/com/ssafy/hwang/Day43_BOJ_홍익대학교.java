c054ed0618c4e70c15ec9fbf0e77f5cde444010c
Kwan <ghkdtnghks147@gmail.com>
2022-03-23 02:12:26 +0900

    Feat "43일차 문제풀이"

---
+++ algorithm/src/com/ssafy/hwang/Day43_BOJ_홍익대학교.java
@@ -0,0 +1,12 @@
+package com.ssafy.hwang;
+
+import java.util.Scanner;
+
+public class Day43_BOJ_홍익대학교 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int i = sc.nextInt();
+		System.out.println(i-1946);
+	}
+}
