f4daa25175eb86742cf2d33b5232d60ea1f29043
namja <24ph00@naver.com>
2022-03-23 16:48:31 +0900

    Feat: "Day44"

---
+++ algorithm/src/com/ssafy/namja/BOJ_2163_박성현.java
@@ -0,0 +1,13 @@
+package com.ssafy.namja;
+
+import java.util.Scanner;
+
+public class BOJ_2163_박성현 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int N = sc.nextInt();
+		int M = sc.nextInt();
+		System.out.println(N * M - 1);
+	}
+}
