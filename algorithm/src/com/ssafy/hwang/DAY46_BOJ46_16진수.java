bea15229c3b309d1d58fd090169ba346d78b3ce9
Kwan <ghkdtnghks147@gmail.com>
2022-03-26 02:50:16 +0900

    Feat "46일차 문제풀이"

---
+++ algorithm/src/com/ssafy/hwang/DAY46_BOJ46_16진수.java
@@ -0,0 +1,12 @@
+package com.ssafy.hwang;
+
+import java.util.Scanner;
+
+public class DAY46_BOJ46_16진수 {
+
+	    public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+	        int a = sc.nextInt(16);
+	        System.out.println(a);
+	    }
+	}
