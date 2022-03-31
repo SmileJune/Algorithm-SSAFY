5ba05179327fb172841fef43c7f4cef287ec116c
Kwan <ghkdtnghks147@gmail.com>
2022-03-31 22:41:35 +0900

    Feat "52일차 문제풀이"

---
+++ algorithm/src/com/ssafy/hwang/Day52_BOJ_한글2.java
@@ -0,0 +1,10 @@
+package com.ssafy.hwang;
+
+import java.util.Scanner;
+
+public class Day52_BOJ_한글2 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println(sc.next().charAt(0)-44031);
+	}
+}
