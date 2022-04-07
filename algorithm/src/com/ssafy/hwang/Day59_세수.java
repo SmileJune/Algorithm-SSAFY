1ee49d184de144b369aa6777e1efc79078f04bf5
Kwan <ghkdtnghks147@gmail.com>
2022-04-07 22:15:43 +0900

    Feat " 59일차 문제풀이"

---
+++ algorithm/src/com/ssafy/hwang/Day59_세수.java
@@ -0,0 +1,18 @@
+package com.ssafy.hwang;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Day59_세수 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int[] map = new int[3];
+		
+		for(int i=0; i<3;i++) {
+			map[i] = sc.nextInt();
+		}
+		Arrays.sort(map);
+		System.out.println(map[1]);
+	}
+}
