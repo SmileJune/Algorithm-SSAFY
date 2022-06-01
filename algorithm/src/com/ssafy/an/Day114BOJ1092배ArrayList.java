281425db9b5b3d20f7334b7a4eb924cae90b447a
DESKTOP-3O24HHM\hitedin <hitedin@gmail.com>
2022-06-01 10:48:06 +0900

    Feat "Day114 BOJ1092"

---
+++ algorithm/src/com/ssafy/an/Day114BOJ1092배ArrayList.java
@@ -0,0 +1,55 @@
+package com.ssafy.an;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class Day114BOJ1092배ArrayList { // 1092 배
+	static int N, M, ans;
+	static ArrayList<Integer> crn, box;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+
+		N = Integer.parseInt(br.readLine());
+		crn = new ArrayList<>();
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++)
+			crn.add(Integer.parseInt(st.nextToken()));
+		Collections.sort(crn, Collections.reverseOrder());
+
+		M = Integer.parseInt(br.readLine());
+		box = new ArrayList<>();
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < M; i++)
+			box.add(Integer.parseInt(st.nextToken()));
+		Collections.sort(box, Collections.reverseOrder());
+
+		if (box.get(0) > crn.get(0)) // 가장 큰 수를 보고 불가능 체크를 한다.
+			ans = -1;
+		else {
+			ans = 0;
+			while (!box.isEmpty()) {
+				int idx = 0; // 이부분 구선생님 도움.. 원래 pq로 box 만들었는데,
+				for (int i = 0; i < N; i++) {
+					if (idx == box.size())
+						break; // for문 중에도 box가 모두 없어졌을 수 있다.
+					if (box.get(idx) <= crn.get(i))
+						box.remove(idx);
+					else {
+						idx++;
+						i--; // 지금 크레인 안썼으니 상자만 인덱스 이동
+					}
+				}
+				ans++;
+			}
+		}
+
+		System.out.println(ans);
+		br.close();
+	}
+}
